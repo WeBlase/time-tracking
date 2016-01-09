34cfbfacdee8456a83fa5298830b6bde113933bf
WeBlase <aleksandr13savkin@gmail.com>
2016-01-09 10:35:39 -0800

    Test Commit

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -1,12 +1,14 @@
 ActiveAdmin.register Task do
   form do |f|
-    f.inputs do
-      f.input :user_id, :label => 'User', :as => :select, :collection => User.all.map{|u| ["#{u.first_name}, #{u.last_name}", u.id]}
+    f.inputs "Create Tasks" do
+      f.input :user_id, :label => 'User', :as => :select, :collection => User.all.map{|user| ["#{user.first_name}, #{user.last_name}", user.id]}
       f.input :project
       f.input :taskname
       f.input :description
       f.input :timestart
       f.input :timeend
     end
+      f.actions
   end
+  permit_params :user, :project, :taskname, :description, :timestart, :timeend
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register User do
+  index do
+    column :first_name
+    column :last_name
+    default_actions
+    end
+  form do |f|
+    f.inputs "Create User" do
+      f.input :first_name
+      f.input :last_name
+    end
+      f.actions
+  end
   permit_params :first_name, :last_name
 end
