b1657a9daa643281bd02e76d298d36130a7a5ae1
aasavkin <aleksandr13savkin@gmail.com>
2016-03-04 15:54:44 +0300

    authorization

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,4 +1,13 @@
-class PersonsController < ApplicationController
-  def profile
+class PersonsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+
 end
